25d83d59f519224c816f2eb59be9863832e66ad2
goarant-l <goarant-l@saint-louis29.net>
2026-01-29 15:20:27 +0100

    ajout nouvelle partie = test ok = cli ok

--- src/sl29/games/_2048.py
+++ src/sl29/games/_2048.py
@@ -18,9 +18,11 @@ def nouvelle_partie() -> Tuple[List[List[int]], int]:
     :return: Une grille TAILLExTAILLE initialisée avec deux tuiles, ainsi que le score à 0.
     :rtype: Tuple[List[List[int]], int]
     """
-    raise NotImplementedError("Fonction nouvelle_partie non implémentée.")
 
-
+    plateau = _creer_plateau_vide()
+    plateau2 = _ajouter_tuile(plateau)
+    plateau3 = _ajouter_tuile(plateau2)
+    return (plateau3,0)
 
 def jouer_coup(plateau: List[List[int]], direction: str) -> tuple[List[List[int]], int, bool]:
     """
